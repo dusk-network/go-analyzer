6b49cf61c35069f6ada56da8db2a24369f85fc4c
Jules de Smit <julesdesmit@gmail.com>
2021-01-25 14:15:16 +0100

    Implement an extensible linter application
    Fixes #1

--- cmd/go-analyzer/main.go
+++ cmd/go-analyzer/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/dusk-network/go-analyzer/pkg/runner"
+	"github.com/urfave/cli/v2"
+)
+
+var CLIFlags = []cli.Flag{
+	&cli.BoolFlag{
+		Name:    "all",
+		Aliases: []string{"a"},
+		Usage:   "Run all custom lints.",
+		Value:   false,
+	},
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	app := &cli.App{
+		Name:      "go-analyzer",
+		Usage:     "Performs custom lint checks on Golang repositories.",
+		Copyright: "Copyright (c) 2021 DUSK",
+		Version:   "0.1.0",
+		Flags:     CLIFlags,
+		Action:    action,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
+	}
+}
+
+func action(ctx *cli.Context) error {
+	if arguments := ctx.Args(); arguments.Len() == 0 {
+		return errors.New("no lints selected. exiting...")
+	}
+
+	if ctx.Bool("all") {
+		if errs := runner.RunAll(); errs != nil {
+			for err := range errs {
+				fmt.Println(err)
+			}
+
+			return errors.New("lint failed")
+		}
+	}
+
+	return nil
 }
